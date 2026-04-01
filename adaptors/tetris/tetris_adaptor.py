9b6c2a28d67b2a8cbb40e3402cb678ed93187f08
Ben Larson <182564645+Bananapudding01@users.noreply.github.com>
2026-04-01 13:05:37 -0700

    started tetris adaptor

--- adaptors/tetris/tetris_adaptor.py
+++ adaptors/tetris/tetris_adaptor.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytesseract
 from PIL import Image
+import pyautogui as gui
 
 class GameAdaptor:
     def __init__(self, env):
@@ -10,7 +11,24 @@ class GameAdaptor:
         print("reset input")
     
     def stepinput(self, action):
-        print("step input")
+
+        gui.PAUSE = 0
+
+        moves = action % 10
+        rotation = (action // 10)
+
+        for i in range(rotation):
+            gui.press("up") 
+
+        if moves < 5:
+            for i in range(moves + 1):
+                gui.press("left")
+        else:
+            for i in range(moves - 4):
+                gui.press("right")
+
+        for i in range(20):
+            gui.press("down")
 
     def rewardinput(self):
         print("reward input")
